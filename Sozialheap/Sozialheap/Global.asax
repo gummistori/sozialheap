d4d56ecc9232b613cbcbb4a870de7d940dd5047a
vilhjalmuri14 <vilhjalmuri14@ru.is>
2015-04-29 14:33:54 +0000

    this is a test

---
+++ Sozialheap/Sozialheap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sozialheap.MvcApplication" Language="C#" %>
---
+++ Sozialheap/Sozialheap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sozialheap.Startup))]
+namespace Sozialheap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
